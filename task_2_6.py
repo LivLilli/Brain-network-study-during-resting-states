9f5c0e58d32a4930f537db857a3ba63349c8dc18
LivLilli <44709394+LivLilli@users.noreply.github.com>
2019-11-12 12:52:35 +0100

    task 2.6

---
+++ task_2_6.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 12 12:33:03 2019
+
+@author: livialilli
+"""
+from task_2_123 import task_2_1
+
+
+
+'''
+Compare the networks obtained with the analysis 1.6 in terms of graph indices.
+'''
+
+
+if __name__=="__main__":
+    # file name
+    file1 = 'files/S003/S003R01.edf'
+    file2 = 'files/S003/S003R02.edf'
+    # density 20%
+    density1 = 0.20
+    # theta rythm
+    theta_range = (4,7)
+    
+    
+    task_2_1(file1, density1, theta_range)
+    task_2_1(file2, density1, theta_range)
+    
+    
