2ffcbeefb5dec1c0b3158b036b5f52befdc56d0a
Livia <lilli.livia@libero.it>
2019-11-14 11:52:50 +0100

    task 4.1

--- task_4_1.py
+++ task_4_1.py
@@ -8,30 +8,31 @@ Determine number and composition (i.e. list of nodes) of the communities
 obtained applying one of the algorithms introduced during the course.
 '''
 
+def task_4_1(file, freq):
+    pdc = PDC(file, freq)
+    weighted_matrix  = pdc.final_pdc_matrix()
+    G = nx.from_numpy_matrix(weighted_matrix)
+    ### USING NETWORKX
+    # PROBLEM: it accepts just undirected graphs
+    
+    # dictionary: for each node the corresponding community
+    # best_partition: compute the partition of the graph nodes which maximises the modularity using the Louvain heuristices
+    comm_dic = dict(community.best_partition(G))
+    # communieties found
+    values = set(list(comm_dic.values()))
+    
+    print('Using: PDC    File:%s \n' % file, 'Number of communities: ', len(values))
+    print('Communities Composition: ',comm_dic)
+    print('\n \n')
+
 file1 = 'data/S003R01.edf'
 file2 = 'data/S003R02.edf'
 # density 20%
 density1 = 0.20
 alpha_freq = (8, 13)
 
-pdc = PDC(file1, alpha_freq)
-weighted_matrix  = pdc.final_pdc_matrix()
-G = nx.from_numpy_matrix(weighted_matrix)
-
-### USING NETWORKX
-# PROBLEM: it accepts just undirected graphs
-
-# dictionary: for each node the corresponding community
-# best_partition: compute the partition of the graph nodes which maximises the modularity using the Louvain heuristices
-comm_dic = dict(community.best_partition(G))
-
-# communieties found
-values = set(list(comm_dic.values()))
-
-print('Number of communities: ',len(values))
-print('Communities Composition: ',comm_dic)
-
-
+task_4_1(file1,alpha_freq)
+task_4_1(file2,alpha_freq)
 '''
 ### USING INFOMAP
 infomapWrapper = infomap.Infomap("--two-level")
