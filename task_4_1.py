965266fba67bbc2211da814963eb8da0a1c4376f
Livia <lilli.livia@libero.it>
2019-11-14 11:46:16 +0100

    task 4.1

--- task_4_1.py
+++ task_4_1.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 
 import networkx as nx
-import matplotlib.pyplot as plt
-import matplotlib.colors as colors
-import infomap
 from PDC import PDC
+import community
 '''
 Determine number and composition (i.e. list of nodes) of the communities
 obtained applying one of the algorithms introduced during the course.
@@ -20,6 +18,22 @@ pdc = PDC(file1, alpha_freq)
 weighted_matrix  = pdc.final_pdc_matrix()
 G = nx.from_numpy_matrix(weighted_matrix)
 
+### USING NETWORKX
+# PROBLEM: it accepts just undirected graphs
+
+# dictionary: for each node the corresponding community
+# best_partition: compute the partition of the graph nodes which maximises the modularity using the Louvain heuristices
+comm_dic = dict(community.best_partition(G))
+
+# communieties found
+values = set(list(comm_dic.values()))
+
+print('Number of communities: ',len(values))
+print('Communities Composition: ',comm_dic)
+
+
+'''
+### USING INFOMAP
 infomapWrapper = infomap.Infomap("--two-level")
 
 print("Building Infomap network from a NetworkX graph...")
@@ -35,3 +49,4 @@ print("\n#node module")
 for node in infomapWrapper.iterTree():
   if node.isLeaf():
     print("{} {}".format(node.physicalId, node.moduleIndex()))
+'''
